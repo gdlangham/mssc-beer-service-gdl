ac77629ef63ccd78af802e767d804d27ea154850
greg.langham <greg.langham@messagegears.com>
2023-01-31 14:03:43 -0600

    add json formatting to beer dto

--- src/main/java/dev/gregl/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/dev/gregl/msscbeerservice/web/model/BeerDto.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,9 +24,13 @@ public class BeerDto {
     private UUID id;
     @Null
     private Integer version;
+
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
+
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -35,6 +40,8 @@ public class BeerDto {
     @Positive
     @NotNull
     private Long upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
